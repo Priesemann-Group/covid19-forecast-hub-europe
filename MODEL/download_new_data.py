9fdbaf62a1ff73695719600317089dd512e75066
Sebastian <sebastian@mohrenclan.de>
2021-03-11 16:03:28 +0100

    Updated requ. and typo in download file

--- MODEL/download_new_data.py
+++ MODEL/download_new_data.py
@@ -2,9 +2,9 @@
 # @Author:        Sebastian B. Mohr
 # @Email:
 # @Created:       2021-03-11 15:55:15
-# @Last Modified: 2021-03-11 15:55:53
+# @Last Modified: 2021-03-11 16:02:54
 # ------------------------------------------------------------------------------ #
-import covid19_inference as cov
+import covid19_inference as cov19
 
 jhu = cov19.data_retrieval.JHU()
 jhu.download_all_available_data(force_download=True)
